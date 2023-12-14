e6365fa06cb83324eb86347f14100e6137717e89
Kaique.s <141377576+testandoooow@users.noreply.github.com>
2023-12-13 22:17:14 -0300

    Update Segunda Questão.c

--- Segunda Questão.c
+++ Segunda Questão.c
@@ -1,60 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include<locale.h>
 
 struct Aluno_{
-	char nome[2][250];
-	char dataDeNascimento[250];
-	float notas[2];
-	float media; 
+    char nome[250];
+    char dataDeNascimento[250];
+    float notas[2];
+    float media;
 };
 
-struct Aluno_ Aluno[2];
+struct Aluno_ Aluno[5];
 
-float calcularMedia(struct Aluno_ *Aluno){
-	int j, i;
-	float soma;
-	for(i = 0; i < 2; i++){
-	for(j = 0; j < 2; j++){
-	soma += Aluno[i].notas[j];	
-	}
-	Aluno[i].media = soma / j;
-	soma = 0;
-	return Aluno[i].media;
+float calcularMedia(struct Aluno_ Aluno){
+    return (Aluno.notas[0] + Aluno.notas[1]) / 2.0;
 }
+
+void verificarAprovacao(struct Aluno_ Aluno){
+    if (Aluno.media >= 7.0) {
+        printf("Aprovado.\n");
+    } else {
+        printf("Reprovado.\n");
+    }
 }
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-	
-	int i, j;
-	float media;
-	
-	for(i = 0; i < 2; i++){
-		printf("Escreva seu nome: ");
-		gets(Aluno[i].nome);
-		
-		printf("Digite sua data de nascimento: ");
-		gets(Aluno[i].dataDeNascimento);
-		
-		for(j = 0; j < 2; j++){
-			printf("Digite a %dª nota: ",j + 1);
-			scanf("%f",&Aluno[i].notas[j]);
+    int i, j;
+
+    for (i= 0; i < 5; i++){
+        printf("Nome do aluno %d: ",i + 1);
+        gets(Aluno[i].nome);
+
+        printf("Data de nascimento: ");
+        gets(Aluno[i].dataDeNascimento);
+        
+        for(j = 0; j < 2; j++){
+        printf("%dª Nota: ",j + 1);
+        scanf("%f",&Aluno[i].notas[j]);
+    }
+        fflush(stdin);
+
+        Aluno[i].media = calcularMedia(Aluno[i]);
+    }
+    
+    system("cls");
+    
+    for (i = 0; i < 5; i++){
+        printf("Nome: %s\n",Aluno[i].nome);
+
+        for(j = 0; j < 2; j++){
+        printf("%dª Nota: %.1f\n",j + 1,Aluno[i].notas[j]);
 		}
-		fflush(stdin);
-	}
-	
-	media = calcularMedia(Aluno);
-	
-	
-	for(i = 0; i < 2; i++){
-		printf("Nome: %s\n",Aluno[i].nome);
-		
-		for(j = 0; j < 2; j++){
-			printf("%dª Nota: %.1f\n",j + 1, Aluno[i].notas[j]);
-		}
-		printf("\n Media: %.1f\n",media);
-	}
-	
-	return 0;
+		printf("Média: %.1f\n",Aluno[i].media);
+        verificarAprovacao(Aluno[i]);
+    }
+
+    return 0;
 }
